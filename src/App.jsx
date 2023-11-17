779d53fe018a2cef593aa2aeb7d20d7bb306e86e
Daniel Evangelista <danicvan@hotmail.com>
2023-11-16 21:55:14 -0300

    Started restaurant system.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
