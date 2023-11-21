150f2e2b4b0993d46dd1bed0200c6fb411fb55a7
Daniel Evangelista <danicvan@hotmail.com>
2023-11-20 21:21:03 -0300

    Creating nav menu.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
+
   return (
-    <h1>Navbar</h1>
-  )
+    <nav className="nav bd-container">
+      <div className="nav__logo">
+        <a href="#" className="nav__link">
+          <img src={logo} alt="MFX" />
+        </a>
+      </div>
+
+      <div className="nav__menu">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              <FontAwesomeIcon icon={faHouse} className="nav__icon" />
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              <FontAwesomeIcon icon={faHouse} className="nav__icon" />
+              <span>Purchase</span>
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              <FontAwesomeIcon icon={faHouse} className="nav__icon" />
+              <span>Products</span>
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              <FontAwesomeIcon icon={faHouse} className="nav__icon" />
+              <span>Label</span>
+            </a>
+          </li>
+        </ul>
+
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              <FontAwesomeIcon icon={faHouse} className="nav__icon" />
+              <span>Settings</span>
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              <FontAwesomeIcon icon={faHouse} className="nav__icon" />
+              <span>Logout</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
