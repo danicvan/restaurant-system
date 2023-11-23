757a0cafa4984924c8bab77f637ed2b8cf015a98
Daniel Camilo <daniel.camilo@ez4.com.br>
2023-11-23 00:44:03 -0300

    Initialize products page.

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -10,64 +10,64 @@ function Dashboard() {
         <h2>Menu</h2>
         <p>Sexta-feira, 12 de Agosto de 2023</p>
         <div className="container__table">
-        <div className="container__title">
-          <h4>Pedidos</h4>
-          <div className="container__filter">
-            <h4>
-              <FontAwesomeIcon icon={faFilter} className="nav__icon" />
-              <span>Filtro</span>
-            </h4>
+          <div className="container__title">
+            <h4>Pedidos</h4>
+            <div className="container__filter">
+              <h4>
+                <FontAwesomeIcon icon={faFilter} className="nav__icon" />
+                <span>Filtro</span>
+              </h4>
+            </div>
           </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Customização</th>
+                <th>Menu Order</th>
+                <th>Pagamento Total</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM125</td>
+                <td><span className="completo">Completo</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM145</td>
+                <td><span className="preparando">Preparando</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM105</td>
+                <td><span className="pendente">Pendente</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM45</td>
+                <td><span className="completo">Completo</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM245</td>
+                <td><span className="completo">Completo</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM435</td>
+                <td><span className="completo">Completo</span></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Customização</th>
-              <th>Menu Order</th>
-              <th>Pagamento Total</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Tabela 10</td>
-              <td>(Order Items)</td>
-              <td>RM125</td>
-              <td><span className="completo">Completo</span></td>
-            </tr>
-            <tr>
-              <td>Tabela 10</td>
-              <td>(Order Items)</td>
-              <td>RM145</td>
-              <td><span className="preparando">Preparando</span></td>
-            </tr>
-            <tr>
-              <td>Tabela 10</td>
-              <td>(Order Items)</td>
-              <td>RM105</td>
-              <td><span className="pendente">Pendente</span></td>
-            </tr>
-            <tr>
-              <td>Tabela 10</td>
-              <td>(Order Items)</td>
-              <td>RM45</td>
-              <td><span className="completo">Completo</span></td>
-            </tr>
-            <tr>
-              <td>Tabela 10</td>
-              <td>(Order Items)</td>
-              <td>RM245</td>
-              <td><span className="completo">Completo</span></td>
-            </tr>
-            <tr>
-              <td>Tabela 10</td>
-              <td>(Order Items)</td>
-              <td>RM435</td>
-              <td><span className="completo">Completo</span></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
       </div>
     </>
   )
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFilter,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Products() {
   return (
     <>
-      <h1>Products</h1>
+      <div className="container">
+        <h2>Product Description</h2>
+        <div className="container__table">
+        <div className="container__categories">
+              <h4>
+                <FontAwesomeIcon icon={faFilter} className="nav__icon" />
+                <span>Gerenciar Categorias</span>
+              </h4>
+            </div>
+          <div className="container__title">
+            <h4>Pedidos</h4>
+            
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Customização</th>
+                <th>Menu Order</th>
+                <th>Pagamento Total</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM125</td>
+                <td><span className="completo">Completo</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM145</td>
+                <td><span className="preparando">Preparando</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM105</td>
+                <td><span className="pendente">Pendente</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM45</td>
+                <td><span className="completo">Completo</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM245</td>
+                <td><span className="completo">Completo</span></td>
+              </tr>
+              <tr>
+                <td>Tabela 10</td>
+                <td>(Order Items)</td>
+                <td>RM435</td>
+                <td><span className="completo">Completo</span></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   )
 }
